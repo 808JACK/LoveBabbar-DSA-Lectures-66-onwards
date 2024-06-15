6d7b99f38291354c521c9b71df8a47d0d8f702f8
Sarthak <146474979+808JACK@users.noreply.github.com>
2024-06-15 11:07:53 +0530

    BinaryTree.java

--- Lecture-66/builtTree1.java
+++ Lecture-66/builtTree1.java
@@ -1,1 +1,63 @@
+//creating tree from inorder and postorder
+import java.util.HashMap;
+import java.util.Map;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class BinaryTree {
+    static int index;
+
+    public static Node solve(int[] in, int[] post, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+        if (inStart > inEnd) {
+            return null;
+        }
+
+        int element = post[index--];
+        Node root = new Node(element);
+        int position = inMap.get(element);
+
+        root.right = solve(in, post, position + 1, inEnd, inMap);
+        root.left = solve(in, post, inStart, position - 1, inMap);
+
+        return root;
+    }
+
+    public static Node buildTree(int[] in, int[] post) {
+        index = post.length - 1; 
+        Map<Integer, Integer> inMap = new HashMap<>();
+        for (int i = 0; i < in.length; i++) {
+            inMap.put(in[i], i);
+        }
+        return solve(in, post, 0, in.length - 1, inMap);
+    }
+
+    
+    public static void printInorder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        printInorder(node.left);
+        System.out.print(node.data + " ");
+        printInorder(node.right);
+    }
+
+    public static void main(String[] args) {
+        int in[] = {9, 3, 15, 20, 7};
+        int post[] = {9, 15, 7, 20, 3};
+
+        Node root = buildTree(in, post);
+
+        
+        printInorder(root);
+    }
+}
 
